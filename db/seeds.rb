1dc78fd135d7e5227a246a6a1433c820b29db94f
Hector Orellana <pulga.p.29@gmail.com>
2016-09-18 17:14:30 -0300

    Agrega una serie en db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# peliculas = [['Gremlins', 'A small town is besieged by some furry and not-so-cute little creatures after a young man ignores the warnings of a wise elder regarding their care and feeding'],
-#              ['The Avengers', 'Earth mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity'],
-#              ['Batman: The Killing Joke', 'As Batman hunts for the escaped Joker, the Clown Prince of Crime attacks the Gordon family to prove a diabolical point mirroring his own fall into madness.']]
-
-
 peliculas = [{nombre: 'Gremlins',
               fecha: Time.new(1984),
               sinopsis: 'A small town is besieged by some furry and not-so-cute little creatures after a young man ignores the warnings of a wise elder regarding their care and feeding'},
@@ -28,4 +23,15 @@ Pelicula.all.each { |pelicula| pelicula.destroy }
 
 peliculas.each do |pelicula|
   Pelicula.create(pelicula)
+end
+
+series = [{nombre: 'Halt and Catch Fire',
+           sinopsis: 'Silicon Valley en los 80'}]
+
+Serie.all.each do |serie|
+  serie.destroy
+end
+
+series.each do |serie|
+  Serie.create serie
 end
