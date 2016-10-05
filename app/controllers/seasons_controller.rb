68fca41d7427a7b44b1e4ec5b2ab87102519a7ab
Hector Orellana <pulga.p.29@gmail.com>
2016-10-05 00:04:26 -0300

    eliminar temporada

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -21,6 +21,20 @@ class SeasonsController < ApplicationController
     redirect_to show_path(@show)
   end
 
+  def destroy
+    @season = Season.find params[:id]
+
+    show = @season.show
+
+    @season.episodes.each do |episode|
+      episode.destroy!
+    end
+
+    @season.destroy!
+
+    redirect_to show
+  end
+
   def show_params
     params.require(:season).permit(:number, :show)
   end
